4821b10417446201a1eee7a3b2def42d44c52167
Milare99 <adeyemidami19@gmail.com>
2023-02-20 07:40:43 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main - A program that prints a line with but function
+ * Return: 0 (Success)
+ */
+int main(viod)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
