a632d5d8f6244c40b6ab980e8dfb8a6b4ac196c4
Milare99 <122886814+Milare99@users.noreply.github.com>
2023-02-20 18:34:12 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 include <stdio.h>
+
 /**
- * main - A program that prints a line with but function
+ * main - Betty point
  * Return: 0 (Success)
  */
-int main(viod)
+int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
