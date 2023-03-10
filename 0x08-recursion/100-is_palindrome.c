a3fb41069964b549ff6b20fa749ff4e83af326a7
Milare99 <adeyemidami19@gmail.com>
2023-03-10 11:43:58 -0800

    Ellu P

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,12 @@
+#include <stdio.h>
 #include "main.h"
-
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
-
 /**
- * strlen_no_wilds - Returns the length of a string,
- *                   ignoring wildcard characters.
- * @str: The string to be measured.
- *
- * Return: The length.
+ * wildcmp - Entry Point
+ * @s1: input
+ * @s2: input
+ * Return: 0
  */
-int strlen_no_wilds(char *str)
+int wildcmp(char *s1, char *s2)
 {
-	int len = 0, index = 0;
-
-	if (*(str + index))
-	{
-		if (*str != '*')
-			len++;
-
-		index++;
-		len += strlen_no_wilds(str + index);
-	}
-
-	return (len);
+	return (0);
 }
